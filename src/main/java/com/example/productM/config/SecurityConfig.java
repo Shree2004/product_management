8fd2a3d9720e42a71095a4d190c437da0a69ef6e
Shree2004 <shreeshinde3007@gmail.com>
2026-08-20 14:35:43 +0530

    Updated security config based on RBAC

--- src/main/java/com/example/productM/config/SecurityConfig.java
+++ src/main/java/com/example/productM/config/SecurityConfig.java
@@ -1,12 +1,10 @@
 package com.example.productM.config;
 
-import com.example.productM.service.CustomUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,15 +13,10 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    private CustomUserDetailsService customUserDetailsService;
-
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)
@@ -46,8 +39,17 @@ public class SecurityConfig {
                         .requestMatchers("/inventory/**")
                         .hasAnyRole("ADMIN", "SUPER_ADMIN")
 
-                        .requestMatchers("/products/**")
+                        .requestMatchers(HttpMethod.GET, "/products/**")
                         .hasAnyRole("USER", "ADMIN", "SUPER_ADMIN")
+
+                        .requestMatchers(HttpMethod.POST, "/products/**")
+                        .hasAnyRole("ADMIN", "SUPER_ADMIN")
+
+                        .requestMatchers(HttpMethod.PUT, "/products/**")
+                        .hasAnyRole("ADMIN", "SUPER_ADMIN")
+
+                        .requestMatchers(HttpMethod.DELETE, "/products/**")
+                        .hasAnyRole("ADMIN", "SUPER_ADMIN")
 
                         .requestMatchers("/addresses/**")
                         .hasRole("USER")
