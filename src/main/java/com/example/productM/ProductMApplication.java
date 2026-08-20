2425e79fbc3fbee163abb500299bbcb6bf560b6f
Shree2004 <shreeshinde3007@gmail.com>
2026-08-20 10:26:21 +0530

    files add

---
+++ src/main/java/com/example/productM/ProductMApplication.java
@@ -0,0 +1,13 @@
+package com.example.productM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/productM/ProductMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
